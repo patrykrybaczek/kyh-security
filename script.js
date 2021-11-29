d4aaa1ee35bc24ce05955f5e44fa3fb91916b822
RickardKYH <rickard.englund@student.kyh.se>
2021-11-29 15:35:20 +0100

    finale

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ toggle.addEventListener("click", () => {
   let toggleTheMenu = document.getElementById("toggle-menu");
   toggleTheMenu.classList.toggle("active-toggle");
 });
+
+const togglebutton = document.getElementById("toggle-person");
+togglebutton.addEventListener('click', ()=>{
+  document.getElementById('drop-s3').classList.toggle('active-p');
+})
+const togglebuttonB = document.getElementById('toggle-bevakning');
+togglebuttonB.addEventListener('click', ()=>{
+  document.getElementById('drop-s2').classList.toggle('active-b');
+
+
+})
+const togglebuttonE = document.getElementById('toggle-egendom');
+togglebuttonE.addEventListener('click',()=>{
+  document.getElementById('drop-s').classList.toggle('active-e')
+})
