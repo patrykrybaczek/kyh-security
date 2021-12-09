d6ad2f836606a1f34f55197d32463931f51329d6
patrykrybaczek <rybaczekpatryk@icloud.com>
2021-12-09 20:28:49 +0100

    Reduced the size of logo, added some animation to existing elements. Wrote the markup and styling for the rest of the page, Still needs mediaquerys for smaller screen sizes.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,10 +1,13 @@
 let logo = document.querySelector('.logo');
+const body = document.querySelector('body');
 let toggle = document.querySelector('#toggleIcon');
 let menu = document.getElementById('navigation-menu');
 let orderServicesbtn = document.querySelector('#orderServicesbtn')
 toggle.addEventListener('click', function(e) {
+    body.classList.toggle('fixed');
     menu.classList.toggle('hidden');
     logo.classList.toggle('hidden');
     toggle.classList.toggle('fa-times');
     orderServicesbtn.classList.toggle('show');
 });
+
